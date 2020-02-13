13f93a0fffab61a69de1c32b88957cbb1cbe6ef6
Alejandro <jandrin107@gmail.com>
2020-02-13 23:44:38 +0100

    changes in view
    changes in early stage

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -32,57 +32,50 @@
     <v-row justify="center">
       <v-dialog v-model="dialog" persistent max-width="600px">
         <template v-slot:activator="{ on }">
-          <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
+          <v-btn color="primary" dark v-on="on">Nueva Reserva</v-btn>
         </template>
         <v-card>
           <v-card-title>
-            <span class="headline">User Profile</span>
+            <span class="headline">Nueva Reserva</span>
           </v-card-title>
           <v-card-text>
             <v-container>
               <v-row>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field label="Legal first name*" required></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    label="Legal last name*"
-                    hint="example of persistent helper text"
-                    persistent-hint
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field label="Email*" required></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field label="Password*" type="password" required></v-text-field>
+                <v-col cols="12" sm="6">
+                  <v-text-field label="Nombre*" required></v-text-field>
                 </v-col>
                 <v-col cols="12" sm="6">
+                  <v-text-field label="Apellidos*" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Telefono*" hint="móvil o fijo (preferentemente móvil)" persistent-hint required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Email" hint="opcional" persistent-hint ></v-text-field>
+                </v-col>
+                
+                <v-col cols="12" sm="6">
                   <v-select
-                    :items="['0-17', '18-29', '30-54', '54+']"
-                    label="Age*"
+                    :items="['Hombre', 'Mujer']"
+                    label="Sexo*"
                     required
                   ></v-select>
                 </v-col>
                 <v-col cols="12" sm="6">
                   <v-autocomplete
-                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                    label="Interests"
+                    :items="['Cortar', 'Afeitar Navaja', 'Arreglar Barba', 'Peinar']"
+                    label="Servicio/s"
                     multiple
                   ></v-autocomplete>
                 </v-col>
               </v-row>
             </v-container>
-            <small>*indicates required field</small>
+            <small>*Campos obligatorios</small>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-            <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">Cancelar</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">Reservar</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
